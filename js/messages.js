ab7ba9fff0b8f7f855697b99de9ad24962cdc1d4
Nikola <nikola.bekcic@yahoo.com>
2012-07-10 23:47:34 +0200

    novi messages

--- js/messages.js
+++ js/messages.js
@@ -30,9 +30,51 @@ function PlaceYouBet() {
     m_ctx.fillStyle = "white";
     m_ctx.font = m_i + "px Impact";
     m_ctx.fillText("PLEASE, PLACE YOUR BETS!", m_cx, m_cy);
-    setTimeout(function () { if (i < 33) { m_ctx.clearRect(0, 0, m_can.width, m_can.height); } }, 13);
-    setTimeout(function () { if (i < 33) { PlaceYouBet(); } else { resetuj() } }, 14);
+    setTimeout(function () { if (m_i < 33) { m_ctx.clearRect(0, 0, m_can.width, m_can.height); } }, 13);
+    setTimeout(function () { if (m_i < 33) { PlaceYouBet(); } else { resetuj() } }, 14);
     setTimeout(function () { $("cancan").hide(); }, 3000);
+
+}
+
+function youWin() {
+    m_won++;
+    m_ctx = clearRect(0, 0, m_can.width, m_can.height);
+    $("#cancan").show();
+
+    m_ctx.beginPath();
+
+
+    m_ctx.textAlign = "center";
+    m_ctx.fillStyle = "white";
+    m_ctx.font = 30 + "px Impact";
+
+    m_ctx.fillText("YOU WIN!", m_cx.width / 2, m_cy.height / 2);
+    setTimeout(function () { if (m_won < 4) { m_ctx.clearRect(0, 0, m_can.width, m_can.height); } }, 200);
+    setTimeout(function () { if (m_won < 4) { youWin(); } else { resetuj(); } }, 500);
+    setTimeout(function () { $("cancan").hide(); }, 3000);
+}
+
+function gameOver() {
+    m_lose++;
+    m_ctx = clearRect(0, 0, m_can.width, m_can.height);
+    $("#cancan").show();
+
+    m_ctx.beginPath();
+
+    m_ctx.textAlign = "center";
+    m_ctx.fillStyle = "white";
+    m_ctx.font = 30 + "px Impact";
+
+     m_ctx.fillText("YOU LOSE!", m_can.width / 2,m_can.height / 2);
+     setTimeout(function () { if (m_lose < 4) { m_ctx.clearRect(0, 0, m_can.width, m_can.height); } }, 200);
+     setTimeout(function () { if (m_lose < 4) { gameOver(); } else { resetuj(); } }, 500);
+     setTimeout(function () { $("cancan").hide(); }, 3000);
+}
+
+function resetuj() {
+    m_i = 0;
+    m_won = 0;
+    m_lose = 0;
 
 
 }
